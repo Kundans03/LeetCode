2c14d8f57395de57de7c26ec981acec4e2bd2bf3
Kundan <133836864+Kundans03@users.noreply.github.com>
2026-08-05 13:07:31 +0530

    Time: 5 ms (77.52%), Space: 63.6 MB (62.88%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -4,9 +4,8 @@
  */
 var isPalindrome = function (x) {
     let m = String(x);
-    let i = 0, j = m.length - 1;
     let isTrue = false;
-    for (let k = 0; k < m.length; k++, i++, j--) {
+    for (let i = 0, j = m.length - 1; i <= j; i++, j--) {
         if (m.charCodeAt(i) === m.charCodeAt(j)) isTrue=true;
          else return false;
     }
