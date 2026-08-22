79826050e8f79ed440db937b1ef9b9004610a7e2
Kundan <133836864+Kundans03@users.noreply.github.com>
2026-08-22 13:21:13 +0530

    Time: 0 ms (100%), Space: 42.9 MB (62.86%) - LeetHub

--- 0059-spiral-matrix-ii/0059-spiral-matrix-ii.java
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.java
@@ -2,57 +2,38 @@ class Solution {
     public int[][] generateMatrix(int n) {
         int[][] spiral = new int[n][n];
 
-        int top = 0;
-        int bottom = n - 1;
-        int left = 0;
-        int right = n - 1;
+        int left = 0, right = n - 1;
+        int top = 0, bottom = n - 1;
 
-        int inputValue = 1;
+        int num = 1;
 
         while (top <= bottom && left <= right) {
-            inputValue = row(left, right + 1, top, false, spiral, inputValue);
+            for (int col = left; col <= right; col++) {
+                spiral[top][col] = num++;
+            }
             top++;
 
-            inputValue = col(top, bottom + 1, right, false, spiral, inputValue);
+            for (int row = top; row <= bottom; row++) {
+                spiral[row][right] = num++;
+            }
             right--;
 
             if (top <= bottom) {
-                inputValue = row(left, right + 1, bottom, true, spiral, inputValue);
+                for (int col = right; col >= left; col--) {
+                    spiral[bottom][col] = num++;
+                }
                 bottom--;
             }
 
             if (left <= right) {
-                inputValue = col(top, bottom + 1, left, true, spiral, inputValue);
+                for (int row = bottom; row >= top; row--) {
+                    spiral[row][left] = num++;
+                }
                 left++;
             }
         }
 
         return spiral;
-    }
 
-    static int row(int start, int end, int row, boolean isReverse, int[][] spiral, int inputValue) {
-        if (isReverse) {
-            for (int i = end - 1; i >= start; i--) {
-                spiral[row][i] = inputValue++;
-            }
-        } else {
-            for (int i = start; i < end; i++) {
-                spiral[row][i] = inputValue++;
-            }
-        }
-        return inputValue;
-    }
-
-    static int col(int start, int end, int col, boolean isReverse, int[][] spiral, int inputValue) {
-        if (isReverse) {
-            for (int i = end - 1; i >= start; i--) {
-                spiral[i][col] = inputValue++;
-            }
-        } else {
-            for (int i = start; i < end; i++) {
-                spiral[i][col] = inputValue++;
-            }
-        }
-        return inputValue;
     }
 }
