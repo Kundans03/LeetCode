cd3a3176a782a1308474ac4bfb26bbb7a5061a97
Kundan <133836864+Kundans03@users.noreply.github.com>
2026-08-24 22:30:45 +0530

    Time: 0 ms (100%), Space: 46.5 MB (14.85%) - LeetHub

--- 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.java
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.java
@@ -1,8 +1,12 @@
 class Solution {
     public char nextGreatestLetter(char[] letters, char target) {
-        int low = 0;
-        int high = letters.length-1;
+        int m = binarySearch(letters, target);
+        return letters[m];
+    }
 
+    public static int binarySearch(char[] letters, char target) {
+        int low = 0;
+        int high = letters.length - 1;
         while (low <= high) {
             int mid = low + (high - low) / 2;
 
@@ -13,6 +17,13 @@ class Solution {
             }
         }
 
-        return letters[low % letters.length];
+        if (low == letters.length) {
+            return 0;
+        } else if (letters[low] == target) {
+            return low + 1;
+        }
+
+        return low;
+
     }
 }
